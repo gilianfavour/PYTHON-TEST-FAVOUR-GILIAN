3787089eb7fb267b40a7b328809e9fe81db15568
gilianfavour <gilianfavour06gmail.com>
2024-10-24 09:56:35 +0300

    test 1

---
+++ question2.py
@@ -0,0 +1,13 @@
+# Question (2)
+# Write a Python program to ask a student to enter their mark scored and it returns
+# the grade obtained according to the following:
+# Percentage >= 90%: Grade A
+# Percentage >= 80%: Grade B
+# Percentage >= 70%: Grade C
+# Percentage >= 60%: Grade D
+# Percentage >= 40%: Grade E
+# Percentage < 40%:  Grade F
+
+def student_mark ():
+    marks = int(input('Enter your mark scored: \n'))
+    
